73c19ef8e2e504173747dfbb93434172430285e4
soura216 <soura216@gmail.com>
2023-12-31 00:37:55 +0530

    push

--- src/app/modules/product-categories/product-categories.page.ts
+++ src/app/modules/product-categories/product-categories.page.ts
@@ -1,8 +1,27 @@
 import { Component } from '@angular/core';
+import { Subscription } from 'rxjs';
+import { APIService } from 'src/app/services/api.service';
 
 @Component({
   selector: 'app-product-categories-page',
   templateUrl: './product-categories.page.html',
   styleUrls: ['./product-categories.page.scss'],
 })
-export class ProductCategoriesPage {}
+export class ProductCategoriesPage {
+  subscription = new Subscription();
+  categoryList: any;
+
+  constructor(private _APIService: APIService) {}
+
+  ngOnInit() {
+    this.subscription.add(
+      this._APIService.categoryList().subscribe((data: any) => {
+        this.categoryList = data.categoryList;
+      })
+    );
+  }
+
+  ngOnDestroy() {
+    this.subscription.unsubscribe();
+  }
+}
--- src/app/services/api.service.ts
+++ src/app/services/api.service.ts
@@ -15,20 +15,41 @@ export class APIService {
 
   private bannerListSubject: BehaviorSubject<any>;
   private bannerList$: Observable<any> | any = null;
+  private categoryListSubject: BehaviorSubject<any>;
+  private categoryList$: Observable<any> | any = null;
 
   constructor(private http: HttpClient) {
     this.bannerListSubject = new BehaviorSubject(1);
+    this.categoryListSubject = new BehaviorSubject(1);
   }
 
   bannerList(): Observable<any> {
     if (!this.bannerList$) {
       this.bannerList$ = from(
-        this.http.get(`${environment.base_url}api/banner/list`, this.httpOptions)
+        this.http.get(
+          `${environment.base_url}api/banner/list`,
+          this.httpOptions
+        )
       );
       this.bannerList$.subscribe((data: any) => {
         this.bannerListSubject.next(data);
       });
     }
     return this.bannerListSubject;
+  }
+
+  categoryList(): Observable<any> {
+    if (!this.categoryList$) {
+      this.categoryList$ = from(
+        this.http.get(
+          `${environment.base_url}api/product-category/category-list`,
+          this.httpOptions
+        )
+      );
+      this.categoryList$.subscribe((data: any) => {
+        this.categoryListSubject.next(data);
+      });
+    }
+    return this.categoryListSubject;
   }
 }
